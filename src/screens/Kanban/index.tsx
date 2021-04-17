e005e57f44475aba6be875413014500b38334c42
Tyrone-web <1273611319@qq.com>
2021-04-17 10:27:03 +0800

    看板和任务拖拽功能实现

--- src/screens/Kanban/index.tsx
+++ src/screens/Kanban/index.tsx
@@ -1,17 +1,22 @@
 import styled from '@emotion/styled';
 import { Spin } from 'antd';
+import { Drop, DropChild, Drag } from 'components/drag-and-drop';
 import { ScreenContainer } from 'components/lib';
+import { useCallback } from 'react';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import { useDocumentTitle } from 'utils';
-import { useKanbans } from 'utils/kanban';
-import { useTasks } from 'utils/task';
+import { useKanbans, useReorderKanban } from 'utils/kanban';
+import { useReorderTask, useTasks } from 'utils/task';
 import { CreateKanban } from './create-kanban';
 import { KanbanColumn } from './kanban-column';
 import { SearchPanel } from './search-panel';
 import { TaskModal } from './task-modal';
 import {
   useKanbanSearchParams,
+  useKanbansQueryKey,
   useProjectInUrl,
   useTaskSearchParams,
+  useTasksQueryKey,
 } from './util';
 
 export const KanbanScreen = () => {
@@ -24,22 +29,95 @@ export const KanbanScreen = () => {
   const { isLoading: taskIsLoading } = useTasks(useTaskSearchParams());
   const isLoading = taskIsLoading || kanbanIsLoading;
 
+  const onDragEnd = useDragEnd();
+
   return (
-    <ScreenContainer>
-      <h1>{currentProject?.name}看板</h1>
-      <SearchPanel />
-      {isLoading ? (
-        <Spin size="large" />
-      ) : (
-        <ColumnsContainer>
-          {kanbans?.map((kanban) => (
-            <KanbanColumn kanban={kanban} key={kanban.id} />
-          ))}
-          <CreateKanban />
-        </ColumnsContainer>
-      )}
-      <TaskModal />
-    </ScreenContainer>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <ScreenContainer>
+        <h1>{currentProject?.name}看板</h1>
+        <SearchPanel />
+        {isLoading ? (
+          <Spin size="large" />
+        ) : (
+          <ColumnsContainer>
+            <Drop type="COLUMN" direction="horizontal" droppableId="kanban">
+              <DropChild style={{ display: 'flex' }}>
+                {kanbans?.map((kanban, index) => (
+                  <Drag
+                    index={index}
+                    key={kanban.id}
+                    draggableId={`kanban${kanban.id}`}
+                  >
+                    <KanbanColumn kanban={kanban} key={kanban.id} />
+                  </Drag>
+                ))}
+              </DropChild>
+            </Drop>
+            <CreateKanban />
+          </ColumnsContainer>
+        )}
+        <TaskModal />
+      </ScreenContainer>
+    </DragDropContext>
+  );
+};
+
+export const useDragEnd = () => {
+  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: allTasks } = useTasks(useTaskSearchParams());
+  const { mutate: reorderKanban } = useReorderKanban(useKanbansQueryKey());
+  const { mutate: reorderTask } = useReorderTask(useTasksQueryKey());
+
+  return useCallback(
+    ({ source, destination, type }: DropResult) => {
+      if (!destination) {
+        return;
+      }
+      // 看板排序
+      if (type === 'COLUMN') {
+        const fromId = kanbans?.[source.index].id;
+        const toId = kanbans?.[destination.index].id || 0;
+        if (!fromId || !toId || fromId === toId) {
+          return;
+        }
+
+        const type = destination.index > source.index ? 'after' : 'before';
+
+        reorderKanban({ fromId, referenceId: toId, type });
+      }
+      // 任务排序
+      if (type === 'ROW') {
+        const fromKanbanId = +source.droppableId;
+        const toKanbanId = +destination.droppableId;
+        if (fromKanbanId === toKanbanId) {
+          return;
+        }
+
+        const fromTask = allTasks?.filter(
+          (task) => task.kanbanId === fromKanbanId
+        )[source.index];
+
+        const toTask = allTasks?.filter((task) => task.kanbanId === toKanbanId)[
+          destination.index
+        ];
+
+        if (fromTask?.id === toTask?.id) {
+          return;
+        }
+
+        reorderTask({
+          fromId: fromTask?.id || 0,
+          referenceId: toTask?.id || 0,
+          fromKanbanId,
+          toKanbanId,
+          type:
+            fromKanbanId === toKanbanId && destination.index > source.index
+              ? 'after'
+              : 'before',
+        });
+      }
+    },
+    [allTasks, kanbans, reorderKanban, reorderTask]
   );
 };
 
--- src/screens/Kanban/kanban-column.tsx
+++ src/screens/Kanban/kanban-column.tsx
@@ -12,26 +12,40 @@ import { Task } from 'types/task';
 import { Mark } from 'components/mark';
 import { useDeleteKanban } from 'utils/kanban';
 import { Row } from 'components/lib';
+import React from 'react';
+import { Drop, DropChild, Drag } from 'components/drag-and-drop';
 
-export const KanbanColumn = ({ kanban }: { kanban: Kanban }) => {
+export const KanbanColumn = React.forwardRef<
+  HTMLDivElement,
+  { kanban: Kanban }
+>(({ kanban, ...props }, ref) => {
   const { data: allTasks } = useTasks(useTaskSearchParams());
   const tasks = allTasks?.filter((task) => task.kanbanId === kanban.id);
 
   return (
-    <Container>
+    <Container {...props} ref={ref}>
       <Row between>
         <h3>{kanban.name}</h3>
-        <More kanban={kanban} />
+        <More kanban={kanban} key={kanban.id} />
       </Row>
       <TasksContainer>
-        {tasks?.map((task) => (
-          <TaskCard task={task} />
-        ))}
+        <Drop type="ROW" direction="vertical" droppableId={String(kanban.id)}>
+          <DropChild>
+            {tasks?.map((task, index) => (
+              <Drag key={task.id} index={index} draggableId={`task${task.id}`}>
+                <div>
+                  <TaskCard task={task} key={task.id} />
+                </div>
+              </Drag>
+            ))}
+          </DropChild>
+        </Drop>
+
         <CreateTaskt kanbanId={kanban.id} />
       </TasksContainer>
     </Container>
   );
-};
+});
 
 const TaskCard = ({ task }: { task: Task }) => {
   const { startEdit } = useTasksModal();
